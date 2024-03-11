bf094c5450a1af2c213c2001e28ae56ec60e9a24
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-03-11 17:29:38 +0530

    Added Repository Package and removed lombok

--- src/main/java/com/example/demo/model/StudentDataModel.java
+++ src/main/java/com/example/demo/model/StudentDataModel.java
@@ -1,17 +1,63 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
+
 public class StudentDataModel {
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
     public String Name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAdhar() {
+        return Adhar;
+    }
+
+    public void setAdhar(String adhar) {
+        Adhar = adhar;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public int age;
+
+    public String getUniversity() {
+        return University;
+    }
+
+    public void setUniversity(String university) {
+        University = university;
+    }
+
     public String Adhar;
     public String id;
     public String University;
+
+    public StudentDataModel(String name, int age, String adhar, String id, String university) {
+        this.Name = name;
+        this.age = age;
+        this.Adhar = adhar;
+        this.id = id;
+        this.University = university;
+    }
+
 
 }
--- src/main/java/com/example/demo/repository/StudentDataRepo.java
+++ src/main/java/com/example/demo/repository/StudentDataRepo.java
@@ -1,7 +1,48 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.StudentDataModel;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class StudentDataRepo {
-    private HashMap<Intege>
+    private HashMap<String, StudentDataModel> studentDataMap;
+
+    public StudentDataRepo(){
+        this.studentDataMap = new HashMap<>();
+
+    }
+    public StudentDataModel addStudentData(String name,int age,String Adhar,String id,String university){
+        StudentDataModel studentDataModel = new StudentDataModel(name,age,Adhar,id,university);
+        studentDataMap.put(id,studentDataModel);
+        return studentDataModel;
+    }
+    public StudentDataModel getStudentById(String id){
+        return studentDataMap.getOrDefault(id,null);
+    }
+    public List<StudentDataModel> getAllStudents(){
+        List<StudentDataModel> list = new ArrayList<>();
+        for(StudentDataModel student : studentDataMap.values()){
+            list.add(student);
+        }
+        if(list.isEmpty()){
+            throw new RuntimeException("Students not found");
+        }
+        return list;
+    }
+    public List<StudentDataModel> getAllStudentByUniversity(String university){
+        List<StudentDataModel> list = new ArrayList<>();
+        for(StudentDataModel student : studentDataMap.values()){
+            if(student.getUniversity().equalsIgnoreCase(university)){
+                list.add(student);
+            }
+        }
+        if(list.isEmpty()){
+            throw new RuntimeException("Students not found");
+        }
+        return list;
+    }
+
+
 }
