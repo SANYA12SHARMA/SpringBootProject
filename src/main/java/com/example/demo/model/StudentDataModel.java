be109cd392ab15d0b5db699dfb6c31abfdfd97cb
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-03-11 17:12:39 +0530

    Added Student Data Model

---
+++ src/main/java/com/example/demo/model/StudentDataModel.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class StudentDataModel {
+    public String Name;
+    public int age;
+    public String Adhar;
+    public String University;
+
+}
